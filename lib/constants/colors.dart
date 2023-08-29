8de1fbcaf8e8a160c796b9e4deb223ecb8208e2e
yasinalibd02 <yasinalibd02@gmail.com>
2023-08-29 19:59:14 +0600

    update local storage

---
+++ lib/constants/colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color primaryDarkColor = Color(0xFF8358FF);
+  static const Color secondaryDarkColor = Color(0xFF0D102F);
+  static const Color primaryBGDarkColor = Color(0xFF005454);
+  static const Color primaryDarkScaffoldBackgroundColor = Color(0xFF0D102F);
+  static const Color primaryDarkTextColor = Color(0xFF2B2A29);
+  static const Color secondaryDarkTextColor = Color(0xFF2B2A29);
+
+  static const Color primaryLightColor = Color(0xffEAFBFF);
+  static const Color secondaryLightColor = Color(0xFF1B2C3E);
+  static const Color primaryBGLightColor = Color(0xFF068785);
+  static const Color primaryLightScaffoldBackgroundColor = Color(0xffEAFBFF);
+  static const Color primaryLightTextColor = Color(0xFF263159);
+  static const Color primaryTextColor = Color(0xff2B2A29);
+  static const Color secondaryLightTextColor = Color(0xFF1E9FF2);
+
+  static const Color cardLightTextColor = Color(0xFFD9D9D9);
+
+  static const Color whiteColor = Color(0xFFffffff);
+  static const Color redColor = Color(0xFFDC3A3A);
+  static const Color yellowColor = Color(0xFFF4AB2A);
+  static const Color greenColor = Color(0xFF2D845F);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color currencyColor = Color(0xFF1E9FF2);
+  static const Color thirdColor = Color(0xFFE555E0);
+  static const Color transparent = Colors.transparent;
+  static const Color kDarkBlue = Color(0xFF090943);
+
+  static const Color primaryColor = Color(0xFF13BABA);
+  static const Color onboardBgColor = Color(0xFFF3F3F3);
+  static Color textBorderColor = const Color(0xFF2B2A29).withOpacity(0.2);
+}
